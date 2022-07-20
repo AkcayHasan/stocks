a84ee5a43dc8811a12b0c5f96627ac1e52804642
HasanAkcay <>
2022-07-20 23:34:33 +0300

    csv parser added

--- app/src/main/java/com/example/stocks/data/repository/StockRepositoryImpl.kt
+++ app/src/main/java/com/example/stocks/data/repository/StockRepositoryImpl.kt
@@ -1,7 +1,9 @@
 package com.example.stocks.data.repository
 
+import com.example.stocks.data.csv.CSVParser
 import com.example.stocks.data.local.StockDatabase
 import com.example.stocks.data.mapper.toCompanyListing
+import com.example.stocks.data.mapper.toCompanyListingEntity
 import com.example.stocks.data.remote.StockService
 import com.example.stocks.domain.modal.CompanyListing
 import com.example.stocks.domain.repository.StockRepository
@@ -16,8 +18,9 @@ import javax.inject.Singleton
 @Singleton
 class StockRepositoryImpl @Inject constructor(
     private val api: StockService,
-    private val db: StockDatabase
-): StockRepository {
+    private val db: StockDatabase,
+    private val companyListingsParser: CSVParser<CompanyListing>
+) : StockRepository {
 
     private val dao = db.dao
 
@@ -26,7 +29,7 @@ class StockRepositoryImpl @Inject constructor(
         query: String
     ): Flow<Resource<List<CompanyListing>>> {
         return flow {
-            emit(Resource.Loading())
+            emit(Resource.Loading(isLoading = false))
             val localListings = dao.searchCompanyListing(query)
             emit(Resource.Success(localListings.map {
                 it.toCompanyListing()
@@ -36,20 +39,34 @@ class StockRepositoryImpl @Inject constructor(
             val shouldJustLoadFromCache = !isDbEmpty && !fetchFromRemote
 
             if (shouldJustLoadFromCache) {
-                emit(Resource.Loading(false))
+                emit(Resource.Loading(isLoading = false))
                 return@flow
             }
 
             val remoteListings = try {
-                
+                val response = api.getListings()
+                companyListingsParser.parse(response.byteStream())
             } catch (e: IOException) {
                 // for parsing exceptions
                 e.printStackTrace()
-                emit(Resource.Error("Couldn't load data!"))
+                emit(Resource.Error(message = "Couldn't load data!"))
+                null
             } catch (e: HttpException) {
                 // for invalid response
                 e.printStackTrace()
-                emit(Resource.Error("Couldn't load data!"))
+                emit(Resource.Error(message = "Couldn't load data!"))
+                null
+            }
+
+            remoteListings?.let { listings ->
+                dao.clearCompanyListing()
+                dao.insertCompanyListing(listings.map {
+                    it.toCompanyListingEntity()
+                })
+                emit(Resource.Success(data = dao.searchCompanyListing("").map {
+                    it.toCompanyListing()
+                }))
+                emit(Resource.Loading(isLoading = false))
             }
         }
     }
